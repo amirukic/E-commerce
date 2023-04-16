c07e0bcc42f645b71f8643576dbba22544bd462b
Amir <amir.ukic123@gmail.com>
2023-04-16 16:59:24 +0200

    start-navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Logo from "../images/logo.svg";
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src={Logo} alt="Logo" />
+
+    </div>
+  );
+}
+
+export default Navbar;
