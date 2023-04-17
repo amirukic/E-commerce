80c22b203351aae27b141219e3f1541d22c4fc4e
Amir <amir.ukic123@gmail.com>
2023-04-17 16:23:03 +0200

    upgrade-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import Logo from "../images/logo.svg";
-import "./Navbar.css"
+import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-      <img src={Logo} alt="Logo" />
-
+      <img src={Logo} alt="Logo" style={{height: "20px", }} />
+      <div className="sites">
+        <a href="#">Collections</a>
+        <a href="#">Man</a>
+        <a href="#">Women</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
     </div>
   );
 }
