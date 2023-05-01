cfb21c897d10d0b40c79500e79ede6f98e022c11
Amir <amir.ukic123@gmail.com>
2023-05-01 10:48:52 +0200

    upgrade-main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,20 @@
-import Product from "../images/image-product-1.jpg"
+import Product from "../images/image-product-1.jpg";
 
 function Main() {
-    return ( <main>
-        <img src={Product} alt="" />
-    </main> );
+  return (
+    <main>
+      <img src={Product} alt="Product image" id="product-image"/>
+      <aside className="image-text">
+        <h4>SNEAKER COMPANY</h4>
+        <h1>Fall Limited Edition Sneakers</h1>
+        <p>These low-profile sneakers are your perfect casualwear companion.Featuring a durable rubber outer sole, they'll withstand everything the weather can offer. </p>
+        <h2>$125.00    <p>50%</p></h2>
+        <p>$250.00</p>
+        <div>- 0 +  <button>Add to cart</button></div>
+
+      </aside>
+    </main>
+  );
 }
 
 export default Main;
