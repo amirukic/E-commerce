e8bac0edc2936a0ae96d322fecad324094a0913f
Amir <amir.ukic123@gmail.com>
2023-05-14 12:19:48 +0200

    upgrade-main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
       <aside className="image-text">
         <h4>SNEAKER COMPANY</h4>
         <h1>Fall Limited Edition Sneakers</h1>
-        <p>These low-profile sneakers are your perfect casualwear companion.Featuring a durable rubber outer sole, they'll withstand everything the weather can offer. </p>
+        <p>These low-profile sneaker are your perfect casualwear companion.Featuring a durable rubber outer sole, they'll withstand everything the weather can offer. </p>
         <h2>$125.00    <p>50%</p></h2>
         <p>$250.00</p>
         <div>- 0 +  <button>Add to cart</button></div>
